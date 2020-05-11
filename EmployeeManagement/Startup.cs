413b5fb0d65f5a92b2fbbcfc0294e85d8223fa06
Johnson Thomas <johnson@qburst.com>
2020-05-11 19:25:33 +0530

    code related to account management

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using EmployeeManagement.Models;
 using EmployeeManagement.Security;
 using EmployeeManagement.Services;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -35,9 +37,11 @@ namespace EmployeeManagement
 				options.Password.RequiredLength = 5;
 				options.Password.RequiredUniqueChars = 3;
 				options.Password.RequireNonAlphanumeric = false;
+				options.SignIn.RequireConfirmedEmail = true;
 			})
-			.AddEntityFrameworkStores<AppDbContext>();
-			services.AddMvc().AddXmlSerializerFormatters();
+			.AddEntityFrameworkStores<AppDbContext>().
+			AddDefaultTokenProviders();
+			services.AddMvc().AddXmlSerializerFormatters();			
 			services.AddAuthorization(options =>
 			{
 				options.AddPolicy("DeleteRolePolicy",
@@ -48,8 +52,24 @@ namespace EmployeeManagement
 			services.ConfigureApplicationCookie(options =>
 			{
 				options.AccessDeniedPath = new PathString("/Administration/AccessDenied");
+			});			
+			services.AddAuthentication().AddGoogle(o =>
+			{
+				o.ClientId = _config["ExternalAuthSettings:GoogleClientId"] ;
+				o.ClientSecret = _config["ExternalAuthSettings:GoogleClientSecret"];
+				o.UserInformationEndpoint = "https://www.googleapis.com/oauth2/v2/userinfo";
+				o.ClaimActions.Clear();
+				o.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+				o.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+				o.ClaimActions.MapJsonKey(ClaimTypes.GivenName, "given_name");
+				o.ClaimActions.MapJsonKey(ClaimTypes.Surname, "family_name");
+				o.ClaimActions.MapJsonKey("urn:google:profile", "link");
+				o.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
+			}).AddFacebook(options =>
+			{
+				options.AppId = _config["ExternalAuthSettings:FacebookClientId"];
+				options.AppSecret = _config["ExternalAuthSettings:FacebookClientSecret"]; ;
 			});
-
 			services.AddSingleton<IAuthorizationHandler,CanEditOnlyOtherAdminRolesAndClaimsHandler>();
 			// Register the second handler
 			services.AddSingleton<IAuthorizationHandler, SuperAdminHandler>();
@@ -74,7 +94,7 @@ namespace EmployeeManagement
 			//app.Run(async (context) =>
 			//{
 			//await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName + _config["APIKey"]);
-			//});
+			//});			
 			app.UseAuthentication();
 			app.UseMvc(routes =>
 			{
